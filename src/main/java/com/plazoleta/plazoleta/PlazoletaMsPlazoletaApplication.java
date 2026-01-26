bca39b9418d3346d4afad815284d592a20a7d77e
jucaicedoa <jucaicedoa@unal.edu.co>
2026-01-26 11:31:15 -0500

    first commit

---
+++ src/main/java/com/plazoleta/plazoleta/PlazoletaMsPlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.plazoleta.plazoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazoletaMsPlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazoletaMsPlazoletaApplication.class, args);
+	}
+
+}
