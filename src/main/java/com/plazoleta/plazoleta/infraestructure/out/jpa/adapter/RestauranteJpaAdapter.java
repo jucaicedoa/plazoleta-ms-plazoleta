9d7a67855e1da4ad019ae2ec66990720b8a19c18
jucaicedoa <jucaicedoa@unal.edu.co>
2026-01-26 22:15:48 -0500

    feat: agrego adapter jpa

---
+++ src/main/java/com/plazoleta/plazoleta/infraestructure/out/jpa/adapter/RestauranteJpaAdapter.java
@@ -0,0 +1,23 @@
+package com.plazoleta.plazoleta.infraestructure.out.jpa.adapter;
+
+import com.plazoleta.plazoleta.domain.model.Restaurante;
+import com.plazoleta.plazoleta.domain.spi.RestaurantePersistencePort;
+import com.plazoleta.plazoleta.infraestructure.out.jpa.entity.RestauranteEntity;
+import com.plazoleta.plazoleta.infraestructure.out.jpa.mapper.RestauranteEntityMapper;
+import com.plazoleta.plazoleta.infraestructure.out.jpa.repository.RestauranteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RestauranteJpaAdapter implements RestaurantePersistencePort {
+
+    private final RestauranteRepository repository;
+    private final RestauranteEntityMapper mapper;
+
+    @Override
+    public void save(Restaurante restaurant) {
+        RestauranteEntity entity = mapper.toEntity(restaurant);
+        repository.save(entity);
+    }
+}
