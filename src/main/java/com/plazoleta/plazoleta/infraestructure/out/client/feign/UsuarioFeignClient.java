cc90bbfc06ebd85c53eceffabeb17b4fac29e65f
jucaicedoa <jucaicedoa@unal.edu.co>
2026-01-26 22:29:27 -0500

    feat: agrego feign

---
+++ src/main/java/com/plazoleta/plazoleta/infraestructure/out/client/feign/UsuarioFeignClient.java
@@ -0,0 +1,13 @@
+package com.plazoleta.plazoleta.infraestructure.out.client.feign;
+
+import com.plazoleta.plazoleta.infraestructure.out.client.dto.UsuarioResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service", url = "${feign.client.config.user-service.url}")
+public interface UsuarioFeignClient {
+
+    @GetMapping("/usuarios/{id}")
+    UsuarioResponseDto getUserById(@PathVariable("id") Integer id);
+}
