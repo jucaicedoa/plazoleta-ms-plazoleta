bf6c78c6afe28e39a5312483145318cad6119875
jucaicedoa <jucaicedoa@unal.edu.co>
2026-01-28 13:51:13 -0500

    feat: obtener plato por id

--- src/main/java/com/plazoleta/plazoleta/domain/spi/PlatoPersistencePort.java
+++ src/main/java/com/plazoleta/plazoleta/domain/spi/PlatoPersistencePort.java
@@ -3,5 +3,6 @@ package com.plazoleta.plazoleta.domain.spi;
 import com.plazoleta.plazoleta.domain.model.Plato;
 
 public interface PlatoPersistencePort {
+    Plato getById(Long platoId);
     void save(Plato plato);
 }
