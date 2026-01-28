3773f9d093fda24bc8b72d56928832cfbf53f0be
jucaicedoa <jucaicedoa@unal.edu.co>
2026-01-28 10:28:14 -0500

    feat: agrego adapters y ajusto bean

--- src/main/java/com/plazoleta/plazoleta/infraestructure/configuration/BeanConfiguration.java
+++ src/main/java/com/plazoleta/plazoleta/infraestructure/configuration/BeanConfiguration.java
@@ -1,8 +1,12 @@
 package com.plazoleta.plazoleta.infraestructure.configuration;
 
+import com.plazoleta.plazoleta.domain.api.PlatoServicePort;
 import com.plazoleta.plazoleta.domain.api.RestauranteServicePort;
+import com.plazoleta.plazoleta.domain.spi.PlatoPersistencePort;
 import com.plazoleta.plazoleta.domain.spi.RestaurantePersistencePort;
+import com.plazoleta.plazoleta.domain.spi.RestauranteValidationPort;
 import com.plazoleta.plazoleta.domain.spi.UsuarioValidationPort;
+import com.plazoleta.plazoleta.domain.usecase.CrearPlatoUseCase;
 import com.plazoleta.plazoleta.domain.usecase.CrearRestauranteUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -12,11 +16,22 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class BeanConfiguration {
 
-    private final RestaurantePersistencePort restaurantPersistencePort;
-    private final UsuarioValidationPort userValidationPort;
+    private final RestaurantePersistencePort restaurantePersistencePort;
+    private final UsuarioValidationPort usuarioValidationPort;
+    private final PlatoPersistencePort platoPersistencePort;
+    private final RestauranteValidationPort restauranteValidationPort;
 
     @Bean
-    public RestauranteServicePort restaurantServicePort() {
-        return new CrearRestauranteUseCase(restaurantPersistencePort, userValidationPort);
+    public RestauranteServicePort restauranteServicePort() {
+        return new CrearRestauranteUseCase(restaurantePersistencePort, usuarioValidationPort);
+    }
+
+    @Bean
+    public PlatoServicePort dishServicePort() {
+        return new CrearPlatoUseCase(
+                platoPersistencePort,
+                usuarioValidationPort,
+                restauranteValidationPort
+        );
     }
 }
---
+++ src/main/java/com/plazoleta/plazoleta/infraestructure/out/jpa/adapter/RestauranteValidationJpaAdapter.java
@@ -0,0 +1,27 @@
+package com.plazoleta.plazoleta.infraestructure.out.jpa.adapter;
+
+import com.plazoleta.plazoleta.domain.spi.RestauranteValidationPort;
+import com.plazoleta.plazoleta.infraestructure.out.jpa.entity.RestauranteEntity;
+import com.plazoleta.plazoleta.infraestructure.out.jpa.repository.RestauranteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class RestauranteValidationJpaAdapter implements RestauranteValidationPort {
+
+    private final RestauranteRepository restauranteRepository;
+
+    @Override
+    public boolean restaurantePerteneceAPropietario(Long restauranteId, Long propietarioId) {
+        Optional<RestauranteEntity> restaurant = restauranteRepository.findById(restauranteId);
+        return restaurant.isPresent() &&
+                restaurant.get().getPropietarioId().equals(propietarioId);
+    }
+
+    @Override
+    public boolean restauranteExiste(Long restauranteId) {
+        return restauranteRepository.existsById(restauranteId);
+    }
+}
